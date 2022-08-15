f2c4292d3789c9b697821f7f28756dd234520ffe
ap3ufersa <ap3ufersa@gmail.com>
2022-08-15 11:05:45 -0300

    Corrigindo exercício 1.
    Prezada veja com muita atenção minhas correções por favor. Há muitos erros que não deveriam ter sido cometidos por alguém que passou por ap1 e ap2.

---
+++ TestaPessoa.java
@@ -0,0 +1,13 @@
+/*pachage br.com.ap3.adna;
+
+\\TestaPessoa
+\\br.com.ap3.adna.Pessoa
+*/
+
+public class TestaPessoa {
+    // public static void main(
+    // system.out.println(adna)
+    public static void main(String[] args) {
+        System.out.println("Adna");
+    }
+}
